e54c86924bab04e09ef00dc1f9f54663a867dbbd
ZenKyros <roscod108@gmail.com>
2026-09-14 23:25:07 +0530

    setup conmplete Llama Index

--- Llama Index/test.py
+++ Llama Index/test.py
@@ -1,10 +1,21 @@
-import os 
-from langchain_ollama import ChatOllama
+from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
+import os
+from dotenv import load_dotenv
 
-model = ChatOllama(
-    model = "qwen3:4b"
+# Load the .env file
+load_dotenv()
+
+# Retrieve HF_TOKEN from the environment variables
+hf_token = os.getenv("HF_TOKEN")
+
+llm = HuggingFaceInferenceAPI(
+    model_name="Qwen/Qwen2.5-Coder-32B-Instruct",
+    temperature=0.7,
+    max_tokens=100,
+    token=hf_token,
+    provider="auto"
 )
 
-result = model.invoke("What is Paper?")
-
-print(result)
+response = llm.complete("Hello, how are you?")
+print(response)
+# I am good, how can I help you today?
